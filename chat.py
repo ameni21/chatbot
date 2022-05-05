b849f19459ec5b391bcfe9d68ab8eb3f37a6e91a
Ameni mhadhbi <mhadhbameni@gmail.com>
2022-05-05 16:43:31 +0100

    make it better

--- chat.py
+++ chat.py
@@ -4,7 +4,6 @@ import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as json_data:
